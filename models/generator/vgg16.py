5f41e311e5eb240542a0939f8601cf49129a45d5
daisybby2 <2720484671@qq.com>
2023-04-04 20:15:26 +0800

    对VGG16进行了一点修改，第一层的输入3通道，修改成了单通道，可以训练

--- models/generator/vgg16.py
+++ models/generator/vgg16.py
@@ -11,6 +11,16 @@ class VGG16FeatureExtractor(nn.Module):
 
         vgg16 = torchvision.models.vgg16(pretrained=True)
 
+        ##把VGG16的输入由3通道修改成单通道
+        weight = vgg16.features[0].weight.sum(dim=1, keepdim=True)
+        bias = vgg16.features[0].bias
+        print(weight.shape)
+        vgg16.features[0] = nn.Conv2d(1, 64, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        vgg16.features[0].weight = nn.Parameter(weight)
+        vgg16.features[0].bias = nn.Parameter(bias)
+        print(vgg16.features[0].weight.requires_grad)
+        print(vgg16.features[0].bias.requires_grad)
+
         self.enc_1 = nn.Sequential(*vgg16.features[:5])
         self.enc_2 = nn.Sequential(*vgg16.features[5:10])
         self.enc_3 = nn.Sequential(*vgg16.features[10:17])
